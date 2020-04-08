d5b7c42398559777ca5f4ae46094b23a03717ba9
Eliza Grames <eliza.grames@uconn.edu>
2020-04-08 12:28:36 -0400

    suggested skeleton script for what can be done with subsetted sections

---
+++ R/code_sections.R
@@ -0,0 +1,13 @@
+# This script is a catch-all for all the fun things you can do with articles once sections have been subsetted
+# My suggestions below all relate to the methods section since it is likely most useful for screening and coding articles
+
+# Topic modeling ####
+
+
+# Re-applying search terms ####
+
+
+# Geocoding study sites ####
+
+
+# Coding features from an ontology ####
