391018a8a8f4b641539b0bf3d4e77a78ca7b7c1b
Eliza Grames <eliza.grames@uconn.edu>
2020-04-08 11:30:49 -0400

    skeleton for scraping functions

---
+++ R/scraping_html.R
@@ -0,0 +1,23 @@
+# This script is for functions that scrape/read html as the first part of the doi2txt workflow
+# Please document/annotate new functions as you write them
+
+# wrapper function to combine all subfunctions
+read_html <- function(doi=NULL, title=NULL, year=NULL, author=NULL){
+
+}
+
+
+# Functions for scraping html from a doi ####
+
+doi2html <- function(doi){
+
+}
+
+
+# Functions for looking up doi or scraping html from bibliographic data ####
+
+bib2html <- function(title=NULL, year=NULL, author=NULL){
+
+}
+
+# Subfunctions that address various common scraping issues encountered ####
