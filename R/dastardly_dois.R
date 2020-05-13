d426ec3cc225aca9417fb9b47dad17ce0015eca8
Eliza Grames <eliza.grames@uconn.edu>
2020-05-13 11:14:05 -0400

    updated documentation for dastardly dois

--- R/dastardly_dois.R
+++ R/dastardly_dois.R
@@ -1,26 +1,18 @@
-# This script is for a function used to get a dastardly list of dois to provide to doi2text functions
-
-# Dastardly dois ####
-
 #' Get a list of dastardly dois to cause problems in the doistext functions
 #' @description Using datacite website to extract dois that link to scientific publications (can we extend to reports?)
 #' @param limit A numerical value representing how many dois to return. This number will be larger than those returned because the function selects only the dois that lead to scientific articles
-#' @return A list of dois
-#data not all publications but might be useful in future (and for Living Norway)
-#devtools::install_github("ropensci/rdatacite")
-get_dastardly_dois <- function(limit) {
+#' @param type A string of length 1 indicating the type of document from which to return dois
+#' @return A character vector of dois
+#' @example get_dastardly_dois(limit=10)
+get_dastardly_dois <- function(limit, type="ScholarlyArticle") {
 
 dois_list=rdatacite::dc_dois(
   limit = limit, random=TRUE)
 
 pub_dois<-dois_list$data$attributes
 
-pub_dois<-pub_dois[pub_dois$types$schemaOrg=="ScholarlyArticle",]
+pub_dois<-pub_dois[pub_dois$types$schemaOrg==type,]
 
-pub_dois$doi
+return(pub_dois$doi)
 
 }
-
-#some_dois<-get_dastardly_dois(limit=1000)
-
-#articles <- lapply(some_dois, doi2html)
