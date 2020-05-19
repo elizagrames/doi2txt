cf9a33d791ae51890d8b0bfa3a9c7613b9f61a3a
Matthew Grainger <DrMattG@users.noreply.github.com>
2020-05-19 10:54:38 +0200

    Update code_sections.R
    Added get_cities function - it does the same as the country one

--- R/code_sections.R
+++ R/code_sections.R
@@ -35,4 +35,26 @@ as_tibble(CountryList_raw)
 # #e.g. "Brazil, Brazil, Brazil, Brazil, Brazil, Venezuela, Venezuela, Colombia, Guyana, Brazil, Guyana" 
 # #I want to find a way to group countries together and then count the number of times each is recorded            
 
+#get dataframe of cities mentioned in article section
+
+get_cities_fr_section<-function(section){
+  library(tidyverse)
+  library(maps)
+  ## Loading country data from package maps
+  data(world.cities)
+  all_cities <- str_c(unique(world.cities$name), collapse = "|")
+  ###Remove punctuation
+  raw <- gsub("[[:punct:]\n]","",section)
+  
+  CityList_raw<-sapply(str_extract_all(raw, all_cities), toString)
+  
+  as_tibble(CityList_raw)
+}
+# #Get cities from methods section
+# get_cities_fr_section(methods)
+# #Get cities from articles
+# get_cities_fr_section(articles)
+# 
+# #Challenges - As above - how best to output this. For geocode we probably need a unique list of cities in each row. Also it brings in lots of cities that are just nonsense (in the paper not in the real world)
+
 # Coding features from an ontology ####
