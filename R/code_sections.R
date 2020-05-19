ec98853cbded0fed73f18906018b09bfc37874b2
Matthew Grainger <DrMattG@users.noreply.github.com>
2020-05-19 08:33:11 +0200

    Update code_sections.R
    I added a function for getting countries from sections of the paper.

--- R/code_sections.R
+++ R/code_sections.R
@@ -9,5 +9,26 @@
 
 # Geocoding study sites ####
 
+#get dataframe of countries mentioned in article section
+get_countries_fr_section<-function(section){
+library(tidyverse)
+library(maps)
+## Loading country data from package maps
+data(world.cities)
+###Remove punctuation
+raw <- gsub("[[:punct:]\n]","",section)
+# Split data at word boundaries
+raw2 <- strsplit(raw, " ")
+# Match on country in world.countries
+CountryList_raw <- (lapply(raw2, function(x)x[which(toupper(x) %in% toupper(world.cities$country.etc))]))
+plyr::ldply(CountryList_raw, rbind)
+}
+##Examples
+##get a dataframe of all the countries from the methods section
+#get_countries_fr_section(methods)
+##get a dataframe of all the countries from the all sections
+#get_countries_fr_section(articles)
+## Current challenges - countries with two part names "South Africa", "South Korea", etc. get omitted because of the strsplit - do I need to do this??
+                           
 
 # Coding features from an ontology ####
